41e0bdba26595d80ee3015cccbf99b5f23b3b24f
KevinChicoD <143564440+KevinChicoD@users.noreply.github.com>
2023-09-18 16:51:41 -0700

    Add files via upload

---
+++ EVA1_10_INICIALIZACION/src/eva1_10_inicializacion/EVA1_10_INICIALIZACION.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_10_inicializacion;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_INICIALIZACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor = 0;
+        System.out.println(valor);
+
+        int precio; //declarar
+        precio = 100; //inicializar
+
+        double salario = 0, precioMayoreo = 90, iva = 2.67, subtotal = 90, total = 98; //5 variables todas detipo double
+
+        //CONSTANTES
+        //Final --> TE PERMITE ASIGNAR UN VALOR SOLO UNA VEZ
+        final int conteo;
+        conteo = 90;
+        System.out.println(conteo);
+
+        final double MI_VALOR_PI;
+        MI_VALOR_PI = 3.1416;
+        final String ESCUELA = "Instituto tecnologico de Chihuahua  II";
+        final int CALIFA_PASAR = 70;
+        /*
+            varias lineas de comentarios 
+            cuando quiero tirar mucho rollo
+            esta es la mejor opcion
+        */
+        
+        //COMETARIO DE UNA SOLA LINEA, TODO EN LA LINEA ES IGNORADO
+
+    }
+
+}
